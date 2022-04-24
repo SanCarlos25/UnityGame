0c963f313a33c8996511dd429f6d721e36319ae3
kennygarza11 <61720614+kennygarza11@users.noreply.github.com>
2022-04-23 23:42:40 -0500

    Fixed music

--- Game app/Assets/Scripts/AudioManager.cs
+++ Game app/Assets/Scripts/AudioManager.cs
@@ -6,21 +6,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    private static AudioManager backgroundmusic;
-
+    
     private void Awake()
-    {
-        if (backgroundmusic == null)
-        {
-            backgroundmusic = this;
-            DontDestroyOnLoad(backgroundmusic);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-    /*private void Awake()
     {
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("BGM");
         if (musicObj.Length > 1)
@@ -28,5 +15,5 @@ public class AudioManager : MonoBehaviour
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
-    }*/
+    }
 }
