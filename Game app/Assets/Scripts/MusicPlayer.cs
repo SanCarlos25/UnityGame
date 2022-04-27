a60da93960a9ad079a72c57f9b1aca8d4fde8fbd
giagia29 <giacoi2907@gmail.com>
2022-04-27 16:52:58 -0500

    Modify MusicPlayer.cs

--- Game app/Assets/Scripts/MusicPlayer.cs
+++ Game app/Assets/Scripts/MusicPlayer.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+[RequireComponent(typeof(AudioSource))]
 public class MusicPlayer : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
     // Start is called before the first frame update
-
+    public GameObject backgroundmusic;
+    public AudioSource bgm;
     void Start()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
         {
-            PlayerPrefs.SetFloat("musicVolume", 1f);
+            PlayerPrefs.SetFloat("musicVolume",0.02f);
             Load();
         }
         else
@@ -22,7 +24,14 @@ public class MusicPlayer : MonoBehaviour
 
     public void ChangeVolume()
     {
-        AudioListener.volume = volumeSlider.value;
+        if (backgroundmusic == null)
+        {
+            backgroundmusic = GameObject.FindWithTag("BGM");
+        }
+
+        bgm = backgroundmusic.GetComponent<AudioSource>();
+        //AudioListener.volume = volumeSlider.value;
+        bgm.volume = volumeSlider.value;
         Save();
     }
 
